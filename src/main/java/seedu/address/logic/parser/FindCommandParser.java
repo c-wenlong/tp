6c4124e222163320d4446f7a677c84ff0d2ce8ab
evedaktyl <evedaktyl@gmail.com>
2024-04-03 15:48:01 +0800

    Update find command parser
    The parser is lengthy and difficult to read. Let's extract out the parsing logic for each field into an individual method.

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -23,54 +23,121 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        try {
-            String trimmedArgs = args.trim();
-            String[] splitArgs = trimmedArgs.split(" ", 2);
-            String field = splitArgs[0];
-            String argsToMatch = splitArgs[1].trim();
+        String[] parts = args.trim().split("\\s+");
+        String field = parts[0].trim();
+        String argsToMatch = args.trim().substring(field.length()).trim();
 
-            switch (field) {
-            case "name":
-                String[] nameKeywords = argsToMatch.split("\\s+");
-                return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        switch (field) {
+        case "name":
+            return findNameParser(argsToMatch);
 
-            case "major":
-                String mSubString = argsToMatch.trim();
-                return new FindCommand(new MajorContainsSubStringPredicate(mSubString));
+        case "major":
+            return findMajorParser(argsToMatch);
 
-            case "star":
-                String[] starArgs = argsToMatch.split(" ", 2);
-                String sOperator = starArgs[0];
-                Integer sOperand = Integer.parseInt(starArgs[1].trim());
+        case "star":
+            return findStarParser(argsToMatch);
 
-                if (isInvalidOperator(sOperator)) {
-                    throw new ParseException("Invalid operator.");
-                }
+        case "bolt":
+            return findBoltParser(argsToMatch);
 
-                return new FindCommand(new StarWithinBoundsPredicate(sOperator, sOperand));
+        case "tag":
+            return findTagParser(argsToMatch);
 
-            case "bolt":
-                String[] boltArgs = argsToMatch.split(" ", 2);
-                String bOperator = boltArgs[0];
-                Integer bOperand = Integer.parseInt(boltArgs[1].trim());
-
-                if (isInvalidOperator(bOperator)) {
-                    throw new ParseException("Invalid operator.");
-                }
-
-                return new FindCommand(new BoltWithinBoundsPredicate(bOperator, bOperand));
-
-            case "tag":
-                String tSubString = argsToMatch.trim();
-                return new FindCommand(new TagContainsSubStringPredicate(tSubString));
-
-            default:
-                throw new ParseException(
-                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-            }
-        } catch (IndexOutOfBoundsException | NumberFormatException e) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        default:
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand (by name)
+     * and returns a FindCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+    */
+    public FindCommand findNameParser(String args) throws ParseException {
+        if (args.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    FindCommand.getSpecificMessageUsage("name")));
+        }
+        String[] nameKeywords = args.split("\\s+");
+        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand (by major)
+     * and returns a FindCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FindCommand findMajorParser(String args) throws ParseException {
+        if (args.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    FindCommand.getSpecificMessageUsage("major")));
+        }
+        return new FindCommand(new MajorContainsSubStringPredicate(args));
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand (by star)
+     * and returns a FindCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FindCommand findStarParser(String args) throws ParseException {
+        try {
+            String[] starArgs = args.split(" ", 2);
+            String sOperator = starArgs[0];
+            Integer sOperand = Integer.parseInt(starArgs[1].trim());
+
+            if (isInvalidOperator(sOperator) || sOperand < 0) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        FindCommand.getSpecificMessageUsage("star")));
+            }
+
+            return new FindCommand(new StarWithinBoundsPredicate(sOperator, sOperand));
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    FindCommand.getSpecificMessageUsage("star")));
+        }
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand (by bolt)
+     * and returns a FindCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FindCommand findBoltParser(String args) throws ParseException {
+        try {
+            String[] boltArgs = args.split(" ", 2);
+            String bOperator = boltArgs[0];
+            Integer bOperand = Integer.parseInt(boltArgs[1].trim());
+
+            if (isInvalidOperator(bOperator) || bOperand < 0) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        FindCommand.getSpecificMessageUsage("bolt")));
+            }
+
+            return new FindCommand(new BoltWithinBoundsPredicate(bOperator, bOperand));
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    FindCommand.getSpecificMessageUsage("bolt")));
+        }
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand (by tag)
+     * and returns a FindCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FindCommand findTagParser(String args) throws ParseException {
+        if (args.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    FindCommand.getSpecificMessageUsage("tag")));
+        }
+        return new FindCommand(new TagContainsSubStringPredicate(args));
     }
 
     /**
